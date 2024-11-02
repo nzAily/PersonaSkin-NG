b0ffcb2f6dc1c63612b736334ed10e9e7b63bb20
nzAily <winsdesign.wd@gmail.com>
2024-11-02 10:47:20 +0700

    NG PM Support

--- src/kim/present/personaskin/Main.php
+++ src/kim/present/personaskin/Main.php
@@ -13,7 +13,7 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author  PresentKim (debe3721@gmail.com)
+ * @author  PresentKim
  * @link    https://github.com/PresentKim
  * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
  *
@@ -28,6 +28,7 @@ namespace kim\present\personaskin;
 
 use pocketmine\network\mcpe\convert\SkinAdapter;
 use pocketmine\network\mcpe\convert\TypeConverter;
+use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
@@ -43,11 +44,14 @@ class Main extends PluginBase{
         if(is_dir($dataFolder) && count(scandir($dataFolder)) <= 2){
             rmdir($dataFolder);
         }
-
-        $typeConverter = TypeConverter::getInstance();
-
-        $this->originalAdaptor = $typeConverter->getSkinAdapter();
-        $typeConverter->setSkinAdapter(new PersonaSkinAdapter());
+        foreach (defined(ProtocolInfo::class . "::ACCEPTED_PROTOCOL") ? ProtocolInfo::ACCEPTED_PROTOCOL : [ProtocolInfo::CURRENT_PROTOCOL] as $protocolId) {
+            if (isset($this->states[$protocolId])) {
+                continue;
+            }
+            $typeConverter = TypeConverter::getInstance($protocolId); 
+            $this->originalAdaptor = $typeConverter->getSkinAdapter();
+            $typeConverter->setSkinAdapter(new PersonaSkinAdapter());
+        }
     }
 
     protected function onDisable() : void{
